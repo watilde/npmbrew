e66b42f97befd1e2e6e22ed4e6b350bb7d7d2031
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-03-03 15:23:32 +0900

    Add test the case of use and uninstall

---
+++ test/03-use-v2.7.0.js
@@ -0,0 +1,12 @@
+var fs = require("fs")
+var test = require("tap").test
+var npmbrew = require("../lib/")
+var dirname = require("../lib/config").dirname
+var version = "2.7.0"
+
+test("npmbrew use v2.7.0", function (t) {
+  npmbrew.use(version)
+  var lstat = fs.lstatSync(dirname.current)
+  t.ok(lstat.isSymbolicLink(), "npmbrew has current package")
+  t.end()
+})

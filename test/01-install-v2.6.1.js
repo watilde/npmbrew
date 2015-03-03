0eef066d620c09ee575b175e0713a6c70242f2e7
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-03-03 15:23:22 +0900

    Minor tweaks to test code

--- test/01-install-v2.6.1.js
+++ test/01-install-v2.6.1.js
@@ -4,7 +4,7 @@ var npmbrew = require("../lib/")
 var dirname = require("../lib/config").dirname
 var version = "2.6.1"
 
-test("install v2.6.1", function (t) {
+test("npmbrew install v2.6.1", function (t) {
   npmbrew.install(version, function () {
     var srcFile = dirname.src + "/npm-" + version
     var npmFile = dirname.npm + "/v" + version
--- test/02-install-v2.7.0.js
+++ test/02-install-v2.7.0.js
@@ -4,7 +4,7 @@ var npmbrew = require("../lib/")
 var dirname = require("../lib/config").dirname
 var version = "2.7.0"
 
-test("install v2.7.0", function (t) {
+test("npmbrew install v2.7.0", function (t) {
   npmbrew.install(version, function () {
     var srcFile = dirname.src + "/npm-" + version
     var npmFile = dirname.npm + "/v" + version

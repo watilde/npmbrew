008fc0c02f1cd62d6c6d4489a7503b64b07b3378
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-03-03 15:10:14 +0900

    Hello test codes

---
+++ test/99-clean.js
@@ -0,0 +1,18 @@
+var fs = require("fs")
+var async = require("async")
+var test = require("tap").test
+var npmbrew = require("../lib/")
+var dirname = require("../lib/config").dirname
+var versions = ["2.6.1", "2.7.0"]
+
+test("last: npmbrew clean", function (t) {
+  npmbrew.clean(function () {
+    versions.forEach(function (version) {
+      var srcFile = dirname.src + "/npm-" + version
+      var npmFile = dirname.src + "/v" + version
+      t.notOk(fs.existsSync(srcFile), "v" + version + " src file has cleaned")
+      t.notOk(fs.existsSync(npmFile), "v" + version + " src file has cleaned")
+    })
+    t.end()
+  })
+})

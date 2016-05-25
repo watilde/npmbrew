b5d62866bcb739824a5710775614277948c45c08
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-05-25 14:05:55 +0200

    Fix standard errors

--- test/03-use-v2.7.0.js
+++ test/03-use-v2.7.0.js
@@ -1,12 +1,12 @@
-var fs = require("fs")
-var test = require("tap").test
-var npmbrew = require("../lib/")
-var dirname = require("../lib/config").dirname
-var version = "2.7.0"
+var fs = require('fs')
+var test = require('tap').test
+var npmbrew = require('../lib/')
+var dirname = require('../lib/config').dirname
+var version = '2.7.0'
 
-test("npmbrew use v2.7.0", function (t) {
+test('npmbrew use v2.7.0', function (t) {
   npmbrew.use(version)
   var lstat = fs.lstatSync(dirname.current.npm)
-  t.ok(lstat.isSymbolicLink(), "npmbrew has current package")
+  t.ok(lstat.isSymbolicLink(), 'npmbrew has current package')
   t.end()
 })
